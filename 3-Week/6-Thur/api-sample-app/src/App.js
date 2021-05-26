978d952cc13f86814aac1820a4495ba9ad7c3bd6
nicklonz <nicklonz@gmail.com>
2021-05-25 22:36:45 -0400

    added api-app

---
+++ 3-Week/6-Thur/api-sample-app/src/App.js
@@ -0,0 +1,53 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import Profile from './Profile';
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      userData: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://randomuser.me/api/')
+      .then(data => {
+        console.log(data);
+        this.setState({
+          userData: data.data.results[0]
+        })
+      })
+      .catch(err => {
+        this.setState({
+          errorText: "WHAT ARE YOU THINKING?!"
+        })
+      })
+  }
+
+  getData = () => {
+    axios.get('mytopnewsAPI')
+      .then(topdata => {
+
+      }) 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>User Profile</h1>
+        { this.state.userData 
+            ? <Profile userData={this.state.userData} /> 
+            : <p>Loading...</p> }
+        { this.state.errorText && <p>{this.state.errorText}</p> }
+      </div>
+    );
+  }
+}
+
+export default App;
